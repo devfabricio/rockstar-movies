819d7d786f9e7b72f60aae41ec3db36b0366386e
Fabricio Augusto <fabricio.mata@gmail.com>
2021-02-11 09:39:44 -0800

    feat: add search input on page and handleSearchMovies method

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import { Movie } from '../../services/types'
 import { listMoviesByPopularity } from '../../services/requests'
 import MovieItem from '../../components/Movie/MovieItem'
@@ -9,11 +9,20 @@ const Home = () => {
     listMoviesByPopularity()
       .then(data => setMovies(data.results))
       .catch(error => console.log(error))
-  })
+  }, [])
+
+  const handleSearchMovies = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value)
+  }
+
   return (
     <div className="App">
+      <header>
+        <h1>My Rockstar Movies</h1>
+        <input type={'search'} onChange={(event => handleSearchMovies(event))} />
+      </header>
       <ul>
-        {movies.map((movie, index) => {
+        {movies.map((movie) => {
           return (<li key={movie.id}>
             <MovieItem movie={movie} />
           </li>)
