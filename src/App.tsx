5dd20619ebc181ab4d38051c832e5bd9607bd4ca
Fabricio Augusto <fabricio.mata@gmail.com>
2021-02-11 08:58:19 -0800

    feat: list movies on screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { listMoviesByPopularity } from './services/requests'
+import { Movie } from './services/types'
+import { imgBaseUrl } from './services/api'
 
 const App = () => {
+  const [movies, setMovies] = useState<Movie[]>([])
   useEffect(() => {
     listMoviesByPopularity()
-      .then(data => console.log(data))
+      .then(data => setMovies(data.results))
       .catch(error => console.log(error))
   })
   return (
     <div className="App">
-      This is my rockstar movies baby
+      {movies.map((movie, index) => {
+        return (<li key={movie.id}>
+          <img src={`${imgBaseUrl}/${movie.poster_path}`} alt={movie.title} />
+          <h3>{movie.title}</h3>
+        </li>)
+      })}
     </div>
   )
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,6 @@ const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/'
 })
 
+export const imgBaseUrl = 'https://www.themoviedb.org/t/p/w220_and_h330_face/'
+
 export default api
