27b96bbff78e68f6d2d23c4f87f94c618d3619dd
Fabricio Augusto <fabricio.mata@gmail.com>
2021-02-11 12:56:19 -0800

    feat: add stars icons

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import { Movie } from '../../services/types'
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai'
 import { listMoviesByPopularity } from '../../services/requests'
 import * as S from './styled'
 import MovieItem from '../../components/Movie/MovieItem'
@@ -8,12 +9,24 @@ import Header from '../../components/Layout/Header'
 const Home = () => {
   const [listedMovies, setListedMovies] = useState<Movie[]>([])
   const [searchedMovies, setSearchedMovies] = useState<Movie[]>([])
+  const [ratingStars, setRatingStars] = useState<number[]>(new Array(5).fill(0))
 
   useEffect(() => {
     listMoviesByPopularity()
       .then(data => setListedMovies(data.results))
       .catch(error => console.log(error))
   }, [])
+
+  const handleMouseOverStar = useCallback((index: number) => {
+    const filledStars = new Array(index + 1).fill(1)
+    let i = 0
+    for (const filledStar of filledStars) {
+      console.log(i)
+      ratingStars[i] = filledStar
+      i++
+    }
+    setRatingStars(ratingStars)
+  }, [ratingStars])
 
   const movies = searchedMovies.length > 0
     ? searchedMovies
@@ -23,6 +36,19 @@ const Home = () => {
     <S.Wrapper>
       <Header setSearchedMovies={setSearchedMovies} />
       <S.Content>
+        <div>
+          <h2>Most Popular Movies</h2>
+          <div>Filter by rating:
+            <ul>
+              {ratingStars.map((star: number, index) => {
+                console.log(star)
+                return <li key={index} onMouseEnter={() => handleMouseOverStar(index)}>
+                  {star > 0 ? <AiFillStar /> : <AiOutlineStar />}
+                </li>
+              })}
+            </ul>
+          </div>
+        </div>
         <S.List>
           {movies.map((movie) => {
             return (<MovieItem movie={movie} key={movie.id} />)
