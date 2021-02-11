52b67f29e0dc8251e5670d7a527292b2fd5da920
Fabricio Augusto <fabricio.mata@gmail.com>
2021-02-11 09:50:06 -0800

    feat: add searchMovies method to search movies by query

--- src/services/requests.ts
+++ src/services/requests.ts
@@ -6,3 +6,8 @@ export const listMoviesByPopularity = async (): Promise<MovieList> => {
   const response = await api.get(`/discover/movie?api_key=${key}&sort_by=popularity.desc`)
   return response.data
 }
+
+export const searchMovies = async (query: string): Promise<MovieList> => {
+  const response = await api.get(`/search/movie?api_key=${key}&query=${query}`)
+  return response.data
+}
