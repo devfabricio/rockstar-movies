20b0cc0ba3ea7c3077a5251633eaedd0cdc2a1e1
Fabricio Augusto <fabricio.mata@gmail.com>
2021-02-11 08:25:33 -0800

    test: test request api on console.log

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import { listMoviesByPopularity } from './services/requests'
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    listMoviesByPopularity()
+      .then(data => console.log(data))
+      .catch(error => console.log(error))
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This is my rockstar movies baby
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
